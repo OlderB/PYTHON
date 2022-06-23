a461d9f49897469f7ea503aa3441687599529e13
OlderB <108078885+OlderB@users.noreply.github.com>
2022-06-23 18:10:25 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -4,10 +4,10 @@ Two = input("The second number: ")
 Three = input("Your action(+ or -): ")
 while True:
   if Three == "+":
-    print("Results: ", float(One + Two))
+    print("Results: ", (float(One + Two)))
     break
   elif Three == "-":
-    print("Results: ", float(One - Two))
+    print("Results: ", (float(One - Two)))
     break  
   else:
     print("Again!")
