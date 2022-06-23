000c4aecf457e9c68e59ce2eb2be12e34db8cac4
OlderB <108078885+OlderB@users.noreply.github.com>
2022-06-23 18:05:53 +0300

    Stupid Calculator v1.0
    NOT DESCRIPTION

---
+++ calculator.py
@@ -0,0 +1,13 @@
+One = input("The first number: ")
+Two = input("The second number: ")
+
+Three = input("Your action(+ or -): ")
+while True:
+  if Three == "+":
+    print("Results: "float(One + Two))
+    break
+  elif Three == "-":
+    print("Results: ", float(One - Two))
+    break  
+  else:
+    print("Again!")
